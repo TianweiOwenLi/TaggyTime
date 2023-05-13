a3f95f8cb61d4ae0f50f421b268e0a31688936fb
Owen <tianwei2@andrew.cmu.edu>
2023-05-13 13:19:16 -0400

    Added clap commands for calendar, time, and timezone.

--- src/args.rs
+++ src/args.rs
@@ -1,6 +1,35 @@
 use std::path::PathBuf;
 
-use clap::{Parser, ValueEnum};
+use clap::{Parser, ValueEnum, Subcommand, Args};
+
+#[derive(Subcommand)]
+pub enum TaggyCmd {
+  /// Loads some .ics calendar and gives it a name.
+  CalLoad{
+    /// Path to .ics file
+    path: PathBuf,
+    /// Preferred name of calendar
+    name: String,
+  },
+
+  /// Removes some .ics calendar.
+  CalRm{
+    /// Name of calendar
+    name: String,
+  },
+
+  /// Shows current time.
+  Now,
+
+  /// Shows current timezone.
+  Tz,
+
+  /// Sets Timezone.
+  TzSet{
+    /// Timezone string expression, i.e. -4:00 means EDT.
+    tz_expr: String,
+  }
+}
 
 /// Taggytime mode
 #[derive(Clone)]
@@ -21,4 +50,11 @@ pub struct CliInfo {
 
   // TaggyTime execution mode
   pub mode: Mode,
+
+  // Interaction
+  #[arg(short)]
+  pub interact: bool,
+
+  #[command(subcommand)]
+  pub cmd: TaggyCmd,
 }
