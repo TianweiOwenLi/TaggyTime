e00fcd680c3be1b9d026d712052c6cb89a52a252
Owen <tianwei2@andrew.cmu.edu>
2023-05-17 19:37:28 -0400

    Enabled writing to local directory, thus eliminating the need of typing todo for every taggytime command

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,10 @@ mod time;
 mod util;
 mod util_typs;
 
+use std::path::Path;
+
 use clap::Parser;
-use const_params::TAGGYENV_PATH;
+use const_params::TAGGYENV_RELATIVE_PATH;
 use serde::{Deserialize, Serialize};
 
 use calendar::{cal_event::Event, task::Task, NameMap};
@@ -26,21 +28,25 @@ pub struct TaggyEnv {
 }
 
 /// Loads the interactive environment.
-fn load_env() -> Result<TaggyEnv, TimeError> {
-  let content = std::fs::read_to_string(TAGGYENV_PATH)?;
+fn load_env<P: AsRef<Path>>(path: P) -> Result<TaggyEnv, TimeError> {
+  let content = std::fs::read_to_string(path)?;
   Ok(serde_json::from_str(&content)?)
 }
 
 /// Stores the interactive environment.
-fn store_env(tenv: &TaggyEnv) -> Result<(), TimeError> {
+fn store_env<P: AsRef<Path>>(path: P, tenv: &TaggyEnv) -> Result<(), TimeError> {
   let s = serde_json::to_string(tenv)?;
-  Ok(std::fs::write(TAGGYENV_PATH, s)?)
+  Ok(std::fs::write(path, s)?)
 }
 
 fn main() {
+  let mut tenv_abs_path = home::home_dir().expect("Cannot find home directory! ");
+  tenv_abs_path.push(TAGGYENV_RELATIVE_PATH);
+  println!("path: {}", tenv_abs_path.display());
+
   let cli_info = CliInfo::parse();
 
-  let mut tenv = load_env().unwrap_or_else(|e| {
+  let mut tenv = load_env(&tenv_abs_path).unwrap_or_else(|e| {
     eprintln!("App failed to load taggyenv: \n{:?}", e);
     std::process::exit(1)
   });
@@ -51,7 +57,7 @@ fn main() {
     if let Err(e) = cli_info.cmd.handle(&mut tenv) {
       eprintln!("App encountered error: \n{:?}", e)
     }
-    if let Err(e) = store_env(&tenv) {
+    if let Err(e) = store_env(&tenv_abs_path, &tenv) {
       eprintln!("App failed to save taggyenv: \n{:?}", e)
     }
   }
